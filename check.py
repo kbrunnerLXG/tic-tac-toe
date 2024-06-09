640343767b4e90835d74e11055abe657c01cafab
Aditya <aditya.sharma997@yahoo.in>
2024-06-09 17:57:15 +0800

    tic tac toe ver1

--- check.py
+++ check.py
@@ -1,5 +1,7 @@
 class Check:
     def __init__(self):
+        self.winner_found = None
+        self.winning_comb = None
         self.pos_to_check = None
         self.element_to_check = None
         self.check_ok = None
@@ -11,12 +13,25 @@ class Check:
         if int(self.pos_to_check[0]) <= 3 and int(self.pos_to_check[1]) <= 3:
             if self.element_to_check != 'X' or self.element_to_check != '0':
                 self.check_ok = True
-                return self.check_ok,self.error
+                return self.check_ok, self.error
             else:
                 # print('Invalid position retry')
                 self.error = 'Position already used'
-                return self.check_ok is False,self.error
+                return self.check_ok is False, self.error
         else:
             # print('Invalid position retry')
             self.error = 'Invalid Position'
-            return self.check_ok is False,self.error
+            return self.check_ok is False, self.error
+
+    def check_winner(self, board,inputs):
+        self.winning_comb = [[(0, 0), (0, 1), (0, 2)],
+                             [(0, 0), (1, 0), (2, 0)],
+                             [(0, 0), (1, 1), (2, 2)],
+                             [(0, 2), (1, 2), (2, 2)],
+                             [(0, 2), (1, 1), (2, 0)],
+                             [(2, 0), (2, 1), (2, 2)],
+                             ]
+        for _ in self.winning_comb:
+            result = all(elem in inputs for elem in _)
+            if result:
+                return self.winner_found is True
