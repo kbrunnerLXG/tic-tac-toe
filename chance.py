a7257f3dc406bf1c555faf7b2012305399563d9b
Aditya <aditya.sharma997@yahoo.in>
2024-06-17 14:56:16 +0800

    tic-tac-toe(this one works)

--- chance.py
+++ chance.py
@@ -1,16 +1,19 @@
 class Chance:
     def __init__(self) -> None:
-        pass
+        self.current_player = 'X'
 
-    def get_move(self, board, player):
+    def get_move(self, board):
         while True:
             try:
-                move = int(input(f"Player {player}, enter your move (1-9): ")) - 1
+                move = int(input(f"Player {self.current_player}, enter your move (1-9): ")) - 1
                 row, col = divmod(move, 3)
                 if board[row][col] == " ":
-                    board[row][col] = player
+                    board[row][col] = self.current_player
                     break
                 else:
                     print("This spot is already taken!")
             except (ValueError, IndexError):
                 print("Invalid move. Please enter a number between 1 and 9.")
+
+    def switch_player(self):
+        self.current_player = "O" if self.current_player == "X" else "X"
--- main.py
+++ main.py
@@ -8,21 +8,20 @@ def main():
     board.create_board()
     chance = Chance()
     check = Checker()
-    current_player = "X"
+    # current_player = "X"
     while True:
         board.print_board()
-        chance.get_move(board.current_board, current_player)
-        if check.check_win(board.current_board, current_player):
+        chance.get_move(board.current_board)
+        if check.check_win(board.current_board, chance.current_player):
             board.print_board()
-            print(f"Player {current_player} wins!")
+            print(f"Player {chance.current_player} wins!")
             break
         elif check.check_draw(board.current_board):
             board.print_board()
             print("The game is a draw!")
             break
-
         # Switch player
-        current_player = "O" if current_player == "X" else "X"
+        chance.switch_player()
 
 
 if __name__ == "__main__":
